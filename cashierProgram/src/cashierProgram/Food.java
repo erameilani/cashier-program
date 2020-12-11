0851220971f07504afb4897105882cdc176a670b
erameilani <meilani.era@gmail.com>
2020-12-11 18:47:19 +0700

    change method

--- cashierProgram/src/cashierProgram/Food.java
+++ cashierProgram/src/cashierProgram/Food.java
@@ -1,9 +1,17 @@
 package cashierProgram;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Food implements Interface {
 	
-	private String item;
-	private int price, qty;
+	private String item, orderItem;
+	private int price, quantity, order, qty, orderPrice;
+	private double totalPrice, tax, payment, pay;
+	
+	Scanner input = new Scanner(System.in);
+	ArrayList<Food> foodList = new ArrayList<>();
+	ArrayList<Food> orderList = new ArrayList<>();
 	
 	public Food() {
 	}
@@ -14,11 +22,11 @@ public class Food implements Interface {
 		this.price = price;
 	}
 	
-	public Food(String item, int price, int qty) {
+	public Food(String item, int price, int quantity) {
 		super();
 		this.item = item;
 		this.price = price;
-		this.qty = qty;
+		this.quantity = quantity;
 	}
 
 	public String getItem() {
@@ -29,18 +37,77 @@ public class Food implements Interface {
 		return price;
 	}
 
-	public int getQty() {
-		return qty;
+	public int getQuantity() {
+		return quantity;
 	}
-
-	@Override
-	public void paymentReceipt() {
+	
+	public void listOfFoods() {
+		foodList.add(new Food("Package A (Rice + Fried Chicken + Water)", 20000));
+		foodList.add(new Food("Package B (Rice + Fried Chicken)", 18000));
+		foodList.add(new Food("Fried Chicken", 15000));
+		foodList.add(new Food("Rice", 5000));
+		foodList.add(new Food("Juice", 10000));
+		
+		for (int i = 0; i < foodList.size(); i++) {
+			System.out.println("Item " + (i+1));
+			System.out.println("Food Name : " + foodList.get(i).getItem());
+			System.out.println(String.format("Price     : Rp %,d", foodList.get(i).getPrice()));
+			System.out.println();
+		}
+	}
+	
+	public void listOfOrders() {
 		String strPrice = String.format("Rp %,d", getPrice());
-		String strAmount = String.format("Rp %,d", (getQty() * getPrice()));
+		String strAmount = String.format("Rp %,d", (getQuantity()*getPrice()));
 		
 		System.out.println(getItem());
-		System.out.println(getQty() + " * " + strPrice + " = " + strAmount);
+		System.out.println(getQuantity() + " * " + strPrice + " = " + strAmount);
 		System.out.println();
 	}
+	
+	public void inputOrder() {
+		System.out.print("Add Item  : ");
+		order = input.nextInt();
+		System.out.print("Quantity  : ");
+		qty = input.nextInt();
+		
+		if(order <= foodList.size()) {
+			orderItem = foodList.get(order-1).getItem();
+			orderPrice = foodList.get(order-1).getPrice();
 
+			orderList.add(new Food(orderItem, orderPrice, qty));
+			totalPrice += (orderPrice*qty);
+			
+			System.out.println(String.format("Price     : Rp %,d", orderPrice));
+			System.out.println(String.format("Amount    : Rp %,d", (orderPrice*qty)));
+			System.out.println(String.format("\nTotal     : Rp %,.0f", totalPrice));
+		} else {
+			System.out.println("\nInvalid Item");
+		}
+	}
+	
+	@Override
+	public void paymentReceipt() {
+		for (Food orderFood : orderList) {
+			orderFood.listOfOrders();
+		}
+		
+		tax = totalPrice*0.1;
+		payment = totalPrice*1.1;
+		
+		System.out.println(String.format("Total     : Rp %,9.0f", totalPrice));
+		System.out.println(String.format("Tax       : Rp %,9.0f", tax));
+		System.out.println(String.format("Payment   : Rp %,9.0f", payment));
+		
+		do {
+			System.out.print("\nPay       : ");
+			pay = input.nextDouble();
+			
+			if(pay < payment) System.out.println("Invalid Payment. Try Again");
+		} while (pay < payment);
+		
+		System.out.println(String.format("\nPay       : Rp %,9.0f", pay));
+		System.out.println(String.format("Change    : Rp %,9.0f", (pay-payment)));
+	}
+	
 }
--- cashierProgram/src/cashierProgram/Main.java
+++ cashierProgram/src/cashierProgram/Main.java
@@ -2,91 +2,43 @@ package cashierProgram;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 	
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		ArrayList<Food> foodList = new ArrayList<>();
-		ArrayList<Food> orderList = new ArrayList<>();
+		Food food = new Food();
 		
-		int order, qty, orderPrice;
-		double totalPrice = 0, tax, payment, pay;
-		String orderNumber, orderItem, addMore;
-		
+		int orderNumber;
+		String addMore;
+
 		LocalDateTime date = LocalDateTime.now();
 		DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("E, MMM dd yyyy HH:mm:ss");
 		
-		orderNumber = date.getYear() + "" + date.getMonthValue() + "" + 
-					  date.getDayOfMonth() + "" + date.getHour() + "" +
-					  date.getMinute() + "" + date.getSecond();
+		orderNumber = (int) Math.round(Math.random() * 1000);
 		
 		System.out.println("=====================");
 		System.out.println("   CASHIER PROGRAM   ");
 		System.out.println("=====================");
 		System.out.println("\n*** LIST OF FOODS ***\n");
-		
-		foodList.add(new Food("Package A (Rice + Fried Chicken + Water)", 20000));
-		foodList.add(new Food("Package B (Rice + Fried Chicken)", 18000));
-		foodList.add(new Food("Fried Chicken", 15000));
-		foodList.add(new Food("Rice", 5000));
-		foodList.add(new Food("Juice", 10000));
-		
-		for (int i = 0; i < foodList.size(); i++) {
-			System.out.println("Item " + (i+1));
-			System.out.println("Food Name : " + foodList.get(i).getItem());
-			System.out.println(String.format("Price     : Rp %,d", foodList.get(i).getPrice()));
-			System.out.println();
-		}
-		
+		food.listOfFoods();
+
 		System.out.println("*** ORDER ***\n");
 		do {
-			System.out.print("Add Item  : ");
-			order = input.nextInt();
-			System.out.print("Quantity  : ");
-			qty = input.nextInt();
-			
-			orderItem = foodList.get(order-1).getItem();
-			orderPrice = foodList.get(order-1).getPrice();
-
-			orderList.add(new Food(orderItem, orderPrice, qty));
-			totalPrice += (orderPrice*qty);
-			
-			System.out.println(String.format("Price     : Rp %,d", orderPrice));
-			System.out.println(String.format("Amount    : Rp %,d", (orderPrice*qty)));
-			System.out.println(String.format("\nTotal     : Rp %,.0f", totalPrice));
+			food.inputOrder();
 			
 			System.out.print("\nAdd More ? (y/n) ");
 			addMore = input.next();
 			System.out.println();
-		} while (addMore.equals("y"));
+		} while (addMore.equals("y") || addMore.equals("Y"));
 		
 		System.out.println("*** RECEIPT ***\n");
-		
 		System.out.println("Order No. : " + orderNumber);
 		System.out.println("Date      : " + date.format(dateFormatter));
 		System.out.println();
-		
-		for (Food food : orderList) {
-			food.paymentReceipt();
-		}
-		
-		tax = totalPrice * 0.1;
-		payment = totalPrice * 1.1;
-		
-		System.out.println(String.format("Total     : Rp %,9.0f", totalPrice));
-		System.out.println(String.format("Tax       : Rp %,9.0f", tax));
-		System.out.println(String.format("Payment   : Rp %,9.0f", payment));
-		
-		System.out.print("\nPay       : ");
-		pay = input.nextDouble();
-		
-		System.out.println(String.format("\nPay       : Rp %,9.0f", pay));
-		System.out.println(String.format("Change    : Rp %,9.0f", (pay-payment)));
-		
-		System.out.println("\nThank You, Enjoy Your Meal!");
+		food.paymentReceipt();
+		System.out.println("\nThank You. Enjoy Your Meal!");
 		
 		input.close();
 	}
